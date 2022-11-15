ef39496ec4307e3083f86b721c59b38c3d1bcd9e
Chris Komus <ckomus@gmail.com>
2022-11-14 21:59:24 -0600

    Add Category links to nav menu

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+  before_action :set_categories
+
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+
+  add_flash_types :info, :error, :success
+
+  rescue_from ActionController::Redirecting::UnsafeRedirectError do
+    redirect_to root
+  end
+
+  private
+  def set_categories
+    @categories = Category.all
+  end
 end
